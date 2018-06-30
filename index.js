446c3c95f66151889463ea807854740aa067a78b
Lauren Perchitti <laurenperchitti@gmail.com>
2018-06-30 17:25:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@
      return(`You will see great things from Scuber.`)
    }
   sayHi(){
-    return(`Hi, my name is ${this.name}`)
+    return(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`)
   }
 
  }
