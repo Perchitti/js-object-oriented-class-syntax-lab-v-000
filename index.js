85ec6b281f923582d948d1bc0ad8d99a1dcafcda
Lauren Perchitti <laurenperchitti@gmail.com>
2018-06-30 17:23:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,11 @@
    approve(){
      return(`You can do that!`)
    }
-   
+   doCharity(){
+     return('I like to help people.')
+   }
+   releasePressStatement()[
+     return(`You will see great things from Scuber.`)
+   ]
 
  }
